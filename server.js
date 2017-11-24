9f7583d6604400ca9f66c6d6e1b5b946108d15b3
mohamedmagdysaid <mohamedmagdysaid@gmail.com>
2017-11-24 04:18:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+var os = require("operatingsystem");
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -12,11 +12,18 @@ var app = express();
 app.use(express.static('public'));
 var port = process.env.PORT || 5000
 app.set('port',port);
+var info = os.current();
+ 
+
 app.get('/',function(req,res){
-  var ip = req.connection.remoteAddress;
-  ip = ip.split(":")
-  res.send({"IP Address":ip[3]});
-  console.log(ip[3]);
+  
+
+  var ip = req.headers['x-forwarded-for'].split(",")[0];
+  var lan = req.headers["accept-language"].split(",")[0];
+  var software=req.headers['user-agent'].split(') ')[0].split(' (')[1];
+  
+  var info = {"ipaddress":ip,"Language":lan,"operating system":software}
+  res.send(info);
 });
 
 app.listen(port);
